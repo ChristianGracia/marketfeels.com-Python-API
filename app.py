d344a394ceba159f257f76a6dd8dcfbfa60cbb9b
Christian Gracia <christiangracia.student@careerdevs.com>
2021-01-15 00:21:30 -0500

    added prawn to get reddit posts, git ignore, .env for env vars, and am getting some posts from reddit

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask
+from dotenv import load_dotenv
+
+import praw
+import os
+
+load_dotenv() 
+
 app = Flask(__name__)
 
 @app.route('/')
 def temp_route():
+    return 'coming soon!'
+
+@app.route('/posts')
+def get_posts():
+    reddit = praw.Reddit(client_id = os.getenv('client_id'), client_secret = os.getenv('client_secret'), user_agent = os.getenv('user_agent'))
+
+    hot_posts = reddit.subreddit('MachineLearning').hot(limit=10) 
+
+    list = []
+    for post in hot_posts:
+        print(post.title)
+
     return 'coming soon!'
 
 if __name__ == '__main__':
