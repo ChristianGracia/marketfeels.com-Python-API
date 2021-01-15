a5bfad08a60810f6904b57ba9894f5fa193974a2
Christian Gracia <christiangracia.student@careerdevs.com>
2021-01-15 00:54:08 -0500

    getting some good reddit data now

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json
 from dotenv import load_dotenv
 
 import praw
@@ -16,13 +16,33 @@ def temp_route():
 def get_posts():
     reddit = praw.Reddit(client_id = os.getenv('client_id'), client_secret = os.getenv('client_secret'), user_agent = os.getenv('user_agent'))
 
-    hot_posts = reddit.subreddit('MachineLearning').hot(limit=10) 
+    hot_posts = reddit.subreddit('wallstreetbets').new(limit=20) 
 
-    list = []
+    lists = []
     for post in hot_posts:
-        print(post.title)
+        mini_list = []
+        mini_list.append("title: " + post.title)
+        mini_list.append("description: " + post.selftext)
+        #mini_list.append("comment: " + post.comments)
+        #mini_list.append("likes: " + post.likes)
+        #mini_list.append("category: " + post.category)
+        #mini_list.append("viewcount: " + post.view_count)
+        mini_list.append("score: " + str(post.score))
+        if post.selftext_html is not None:
+            mini_list.append("selftext_html: " + post.selftext_html)
+        mini_list.append("id: " + post.id)
+        mini_list.append("created_utc: " + str(post.created_utc))
+        #mini_list.append("discussion_type: " + post.discussion_type)
+        #mini_list.append("author: " + post.author)
+        
+        lists.append(mini_list)
+        print(dir(post))
 
-    return 'coming soon!'
+    return_string = ""
+
+    for list in lists:
+        return_string += "[" + ", ".join(list) + "]"
+    return return_string
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
