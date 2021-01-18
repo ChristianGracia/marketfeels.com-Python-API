2e9e787c46d42804852ee2e44c12cb06988ddb07
Christian Gracia <christiangracia.student@careerdevs.com>
2021-01-18 00:00:06 -0500

    connect to postgresql db

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, json
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 from services.reddit_service import reddit_api  
+import psycopg2
+from psycopg2 import Error
+import os
 
 
 load_dotenv() 
@@ -10,8 +11,16 @@ load_dotenv()
 app = Flask(__name__)
 app.register_blueprint(reddit_api, url_prefix='/reddit') 
 
-engine = create_engine("postgresql://user:password@localhost:5432/test-db")
-db = scoped_session(sessionmaker(bind=engine))
+print(os.getenv('postgres_user'))
+print(os.getenv('postgres_password'))
+print(os.getenv('postgres_db'))
+
+connection = psycopg2.connect(
+    user=os.getenv('postgres_user'),
+    password=os.getenv('postgres_password'),
+    host="localhost",
+    port="5432",
+    database=os.getenv('postgres_db'))
 
 print("connected to the db!")
 
